4683165db0fb9d8b6456d65736379895563c76de
Michael <Mkoerber7@gmail.com>
2018-04-25 20:54:07 -0400

    Completed 2 katas

--- index.js
+++ index.js
@@ -83,3 +83,34 @@ function sumDigits(number) {
 }
 
 //=======================================================================================================
+
+// Your task is to sort a given string. Each word in the String will contain a single number. This number is the position the word should have in the result.
+
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+// If the input String is empty, return an empty String. The words in the input String will only contain valid consecutive numbers.
+
+function order(words){
+  return words
+    .split(' ')
+    .sort(function(a, b) {
+      return a.match(/\d/) - b.match(/\d/);
+    })
+    .join(' ');
+}
+
+//=======================================================================================================
+
+// Get the number n (n>0) to return the reversed sequence from n to 1.
+
+const reverseSeq = n => {
+  let seq = [];
+    for (var i = n; i >= 1; i--) {
+      seq.push(i);
+    }
+    return seq;
+  };
+
+//=======================================================================================================
+
+
