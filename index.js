16024b268d4ffc5dfe1d4c5cd39fc1a702957a09
Michael <Mkoerber7@gmail.com>
2018-04-28 16:28:07 -0400

    Completed 1 kata

--- index.js
+++ index.js
@@ -140,3 +140,16 @@ function songDecoder(song){
 
 //=======================================================================================================
 
+// To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+
+function openOrSenior(data){
+  let list = [];
+    for (var i=0; i < data.length; i++) {
+      if ((data[i][0] >= 55) && (data[i][1] > 7 )) {
+        list.push('Senior')
+      } else {list.push('Open')}   
+    }
+    return list;
+  }
+
+//=======================================================================================================
