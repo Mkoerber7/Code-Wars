816621c44c383618e002a159241daa926ccdf61c
Michael <Mkoerber7@gmail.com>
2018-05-10 12:02:41 -0400

    Completed 1 kata

--- index.js
+++ index.js
@@ -328,4 +328,12 @@ function isMadhavArray(arr){
 
  //=======================================================================================================
 
- 
+//  Modify the kebabize function so that it converts a camel case string into a kebab case.
+
+
+ function kebabize(str) {
+  return str.replace(/[0-9]/g, '').split(/(?=[A-Z])/).join('-').toLowerCase() 
+}
+
+//=======================================================================================================
+
