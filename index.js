274e757363cc5cacfce9f9d53939ba17b4e8a6e3
Michael <Mkoerber7@gmail.com>
2018-05-11 11:22:08 -0400

    Completed 1 kata

--- index.js
+++ index.js
@@ -337,3 +337,24 @@ function isMadhavArray(arr){
 
 //=======================================================================================================
 
+// You have an array of numbers.
+// Your task is to sort ascending odd numbers but even numbers must be on their places.
+
+// Zero isn't an odd number and you don't need to move it. If you have an empty array, you need to return it.
+
+const sortArray = (array) => {
+  let counter = 0
+  let odds = array.filter( elem => {
+    return elem % 2;
+  })
+  odds.sort((a, b) => a - b);
+  let result = array.map( elem => {
+    if(elem % 2) {
+     counter++;
+     return odds[counter - 1];
+    } else {
+      return elem;
+    }
+  })
+  return result
+}
