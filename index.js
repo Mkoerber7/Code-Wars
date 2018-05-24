b300602fffb3aac37d54583b7121e557bbe21a8e
Michael <Mkoerber7@gmail.com>
2018-05-24 01:54:59 -0400

    Completed A Needle In a Haystack/8Kyu

--- index.js
+++ index.js
@@ -549,3 +549,13 @@ String.prototype.toAlternatingCase = function() {
 //=======================================================================================================
 
 
+// Can you find the needle in the haystack?
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+// After your function finds the needle it should return a message (as a string) that says:
+// "found the needle at position " plus the index it found the needle
+
+function findNeedle(haystack) {
+  let index = haystack.indexOf('needle');// sets the index of 'needle' in every array
+  return `found the needle at position ${index}`;// returns string with the index using string interpolation.
+}
+
