e82ed047c999f328ed94aef0ce69489a15f78eff
Michael <Mkoerber7@gmail.com>
2018-06-01 01:46:52 -0400

    Completed Bin to Decimal/8Kyu

--- index.js
+++ index.js
@@ -720,3 +720,13 @@ var humanYearsCatYearsDogYears = function(humanYears) {
 
 //=======================================================================================================
 
+// 8 kyu
+// Bin to Decimal
+
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+function binToDec(bin){
+  return parseInt(bin, 2); // 
+}
+
+//=======================================================================================================
