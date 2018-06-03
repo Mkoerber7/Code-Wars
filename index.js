6f635e103572a9eaad325f17365553ee0c706b04
Michael <Mkoerber7@gmail.com>
2018-06-03 02:15:27 -0400

    Completed Surface area and volume of a box/8KYU

--- index.js
+++ index.js
@@ -742,3 +742,15 @@ function hexToDec(hexString){
 
 //=======================================================================================================
 
+// 8 kyu
+// Surface Area and Volume of a Box
+
+// Write a function that returns the total surface area and volume of a box as an array: [area, volume].
+
+const getSize = (w, h, d) => {
+  let area = 2*(d*w) + 2*(d*h) + 2*(w*h)
+  let volume = w * h * d
+  return [area, volume];
+};
+
+//=======================================================================================================
