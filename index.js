146dbb8a2a3444d96c7ca01c4ade41e486a8af6f
Michael <Mkoerber7@gmail.com>
2018-06-04 11:27:45 -0400

    Completed Title Case/6Kyu

--- index.js
+++ index.js
@@ -754,3 +754,30 @@ const getSize = (w, h, d) => {
 };
 
 //=======================================================================================================
+
+// 6 kyu
+// Title Case
+
+// A string is considered to be in title case if each word in the string is either (a) capitalised (that is, only the first letter of the word is in upper case) or (b) considered to be an exception and put entirely into lower case unless it is the first word, which is always capitalised.
+
+// Write a function that will convert a string into title case, given an optional list of exceptions (minor words). The list of minor words will be given as a string with each word separated by a space. Your function should ignore the case of the minor words string -- it should behave in the same way even if the case of the minor word string is changed.
+
+function titleCase(title, minorWords) {
+  if (typeof minorWords !== "undefined") {    // checks to see if a minor words argument was even passed through
+    var minorWords = minorWords.toLowerCase().split(' ');
+  } else {
+    var minorWords = []; // returns an empty array if no argument was passed 
+  }
+  
+  return title.toLowerCase().split(' ').map(function(word, i) {
+    if (word != "" && ((minorWords.indexOf(word) === -1) || i == 0)) {
+      word = word.split('');
+      word[0] = word[0].toUpperCase();
+      word = word.join('');
+    }
+    return word;
+  }).join(' ');
+}
+
+//=======================================================================================================
+
