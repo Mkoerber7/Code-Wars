97fd755fcae5908728b9dbdcbbb5729cfff0078c
Michael <Mkoerber7@gmail.com>
2018-06-05 11:27:07 -0400

    Completed Capitalization and Mutability/8kyu

--- index.js
+++ index.js
@@ -781,3 +781,19 @@ function titleCase(title, minorWords) {
 
 //=======================================================================================================
 
+// 8 kyu
+// Capitalization and Mutability
+
+// Your coworker was supposed to write a simple helper function to capitalize a string (that contains a single word) before they went on vacation.
+
+// Unfortunately, they have now left and the code they gave you doesn't work. Fix the helper function they wrote so that it works as intended (i.e. make the first character in the string "word" upper case).
+
+// Don't worry about numbers, special characters, or non-string types being passed to the function. The string lengths will be from 1 character up to 10 characters, but will never be empty.
+
+
+const capitalizeWord = (word) => word.charAt(0).toUpperCase() + word.slice(1);
+// makes the character at index 0 capitalized then adds a copy of the string from the first index on
+
+
+//=======================================================================================================
+
