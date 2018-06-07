a9645d1abb34aeb9dd72cb4130c58ccbf5b79d65
Michael <Mkoerber7@gmail.com>
2018-06-07 18:46:38 -0400

    Completed String repeat, Lost without a map, and string cleaning/8Kyu

--- index.js
+++ index.js
@@ -867,6 +867,40 @@ function betterThanAverage(classPoints, yourPoints) {
 
 //=======================================================================================================
 
+// 8 kyu
+// String repeat
+
+// Write a function called repeatStr which repeats the given string string exactly n times.
+
+const repeatStr = (n, s) => s.repeat(n);
+
+//=======================================================================================================
+
+// 8 kyu
+// Beginner - Lost Without a Map
+
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+// [1, 2, 3] --> [2, 4, 6]
+// For the beginner, try to use the map method - it comes in very handy quite a lot so is a good one to know.
+
+const maps = (x) => x.map((c, e) => c * 2)
+
+//=======================================================================================================
+
+// 8 kyu
+// String cleaning
+
+// Your boss decided to save money by purchasing some cut-rate optical character recognition software for scanning in the text of old novels to your database. At first it seems to capture words okay, but you quickly notice that it throws in a lot of numbers at random places in the text. For example:
+
+// stringClean('! !') == '! !'
+// stringClean('123456789') == ''
+// stringClean('This looks5 grea8t!') == 'This looks great!'
+// Your harried co-workers are looking to you for a solution to take this garbled text and remove all of the numbers. Your program will take in a string and clean out all numeric characters, and return a string with spacing and special characters ~#$%^&!@*():;"'.,? all intact.
+
+const stringClean = (s) => s.replace(/\d+/g, '');
+// replaces any digit 0-9 globally with '',  essentially removing that index.
 
 
-
+//=======================================================================================================
