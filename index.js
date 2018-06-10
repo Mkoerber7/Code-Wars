4e8230dcb5c6a294e20a67173240360f5e1b6c27
Michael <Mkoerber7@gmail.com>
2018-06-10 11:10:22 -0400

    Cmopleted Do I get a bonus?/8Kyu

--- index.js
+++ index.js
@@ -969,3 +969,20 @@ const weatherInfo = f => {
 
 //=======================================================================================================
 
+// 8 kyu
+// Do I get a bonus?
+
+// It's bonus time in the big city! The fatcats are rubbing their paws in anticipation... but who is going to make the most money?
+
+// Build a function that takes in two arguments (salary, bonus). Salary will be an integer, and bonus a boolean.
+
+// If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
+
+// Return the total figure the individual will receive as a string prefixed with "£" (= "\u00A3", JS and Java) or "$" (C#, C++, Ruby, Clojure, Elixir, PHP and Python).
+
+
+const bonusTime = (salary, bonus) => bonus === true ? "\u00A3" + salary * 10 : "\u00A3" + salary;
+
+// If the employee receives a bonus you multiply by ten adding the pound sign, if not just add the pound sign
+
+//=======================================================================================================
