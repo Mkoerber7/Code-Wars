439ac6bfb484dc44b1dbc6a1e45b0f37f911d8f1
Michael <Mkoerber7@gmail.com>
2018-06-20 20:39:00 -0400

    Completed Grasshopper - SUmmation/8kyu

--- index.js
+++ index.js
@@ -1191,3 +1191,12 @@ const otherAngle = (a, b) => 180 - (a+b);
 
 //=======================================================================================================
 
+// 8 kyu
+// Grasshopper - Summation
+
+// Write a program that finds the summation of every number between 1 and num. The number will always be a positive integer greater than 0.
+
+var summation = (num) => num * (num + 1) / 2;
+
+//=======================================================================================================
+
