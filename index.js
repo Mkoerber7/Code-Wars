39a113c4f54789557582af0cc2acc78d261a6a33
Michael <Mkoerber7@gmail.com>
2018-06-21 22:59:10 -0400

    Completed Reversed Words/8Kyu

--- index.js
+++ index.js
@@ -1200,3 +1200,12 @@ var summation = (num) => num * (num + 1) / 2;
 
 //=======================================================================================================
 
+// 8 kyu
+// Reversed Words
+
+// Complete the solution so that it reverses all of the words within the string passed in.
+
+const reverseWords = (str) => str.split(' ').reverse().join(' ');
+
+//=======================================================================================================
+
