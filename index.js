c8270a979a272cef408a8ea380b238ff954355b2
Michael <Mkoerber7@gmail.com>
2018-06-23 11:21:47 -0400

    Completed You Only Need One and Circle Area in 2D/8Kyu / Reached 5 Kyu

--- index.js
+++ index.js
@@ -1209,3 +1209,27 @@ const reverseWords = (str) => str.split(' ').reverse().join(' ');
 
 //=======================================================================================================
 
+// 8 kyu
+// You only need one - Beginner
+
+// You will be given an array (a) and a value (x). All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either. Return true if the array contains the value, false if not.
+
+const check = (a,x) => a.includes(x);
+
+//=======================================================================================================
+
+// 8 kyu
+// Geometry Basics: Circle Area in 2D
+
+// This series of katas will introduce you to basics of doing geometry with computers.
+
+// Write the function circleArea/CircleArea which takes in a Circle object and calculates the area of that circle.
+
+function circleArea({radius}) {
+  return Math.pow(radius,2) * Math.PI;
+}
+
+//=======================================================================================================
+
