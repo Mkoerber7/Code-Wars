2023bc8d9308b3bcf7eabd70fe265c06441fe7d4
Michael <Mkoerber7@gmail.com>
2018-06-27 07:59:11 -0400

    Completed Predict your age/7Kyu

--- index.js
+++ index.js
@@ -1273,3 +1273,23 @@ const disemvowel = (str) => str.replace(/[aeoiu]/gi,'');
 
 //=======================================================================================================
 
+// 7 kyu
+// Predict your age!
+
+// My grandfather always predicted how old people would get, and right before he passed away he revealed his secret!
+
+// In honor of my grandfather's memory we will write a function using his formula!
+
+// Take a list of ages when each of your great-grandparent died.
+// Multiply each number by itself.
+// Add them all together.
+// Take the square root of the result.
+// Divide by two.
+
+const predictAge = (a1,a2,a3,a4,a5,a6,a7,a8) => {
+  let arr = [a1, a2, a3, a4, a5, a6, a7, a8]
+  return Math.floor(Math.sqrt(arr.map((c, i) => c * c).reduce((c, i) => c + i)) / 2);
+}
+
+//=======================================================================================================
+
