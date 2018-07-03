ad3498a9a2847e9f9ca2e1ffef62834b3aa2f15c
Michael <Mkoerber7@gmail.com>
2018-07-03 11:49:50 -0400

    Completed Bumps in the Road and Covfefe/7Kyu

--- index.js
+++ index.js
@@ -1330,3 +1330,33 @@ const stringify = (list) => !list ? "null" : `${list.data} -> ${stringify(list.n
 
 
 //=======================================================================================================
+
+// 7 kyu
+// Bumps in the Road
+
+// Your car is old, it breaks easily. The shock absorbers are gone and you think it can handle about 15 more bumps before it dies totally.
+
+// Unfortunately for you, your drive is very bumpy! Given a string showing either flat road ("_") or bumps ("n"), work out if you make it home safely. 15 bumps or under, return "Woohoo!", over 15 bumps return "Car Dead".
+
+const bump = (x) => {
+  let arr = x.split('').filter((x) => x === "n");
+  return arr.length > 15 ? "Car Dead" : "Woohoo!";
+}
+
+// better solution
+// const bump=x=>x.split('n').length>16?"Car Dead":"Woohoo!"
+
+//=======================================================================================================
+
+// 7 kyu
+// Covfefe
+
+// Your are given a string. You must replace the word(s) coverage by covfefe, however, if you don't find the word coverage in the string, you must add covfefe at the end of the string with a leading space.
+
+// For the languages where the string is not immutable (such as ruby), don't modify the given string, otherwise this will break the test cases.
+
+
+const covfefe = (str) => /coverage/gi.test(str) ? str.replace(/coverage/gi, "covfefe") : str + " covfefe";
+
+
+//=======================================================================================================
