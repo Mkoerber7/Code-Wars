80b399e5a35776aadbb17b7d545b2fea9c67fd14
Michael <Mkoerber7@gmail.com>
2018-07-04 10:25:09 -0400

    Completed are the numbers in order?/7Kyu

--- index.js
+++ index.js
@@ -1360,3 +1360,29 @@ const covfefe = (str) => /coverage/gi.test(str) ? str.replace(/coverage/gi, "cov
 
 
 //=======================================================================================================
+
+// 7 kyu
+// Are the numbers in order?
+
+// In this Kata, your function receives an array of positive integers as input. Your task is to determine whether the numbers are in ascending order.
+
+// For the purposes of this Kata, you may assume that all inputs are valid (i.e. arrays containing only positive integers with a length of at least 2).
+
+const inAscOrder = (arr) => {
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] >= arr[i+1]) { // i + 1 for next value
+      return false; 
+    }    
+  }
+ return true;
+}
+
+// Better solution
+
+// function inAscOrder(arr) {
+//   return arr.every((_,i)=>i==0||arr[i]>arr[i-1]);
+// }
+
+
+//=======================================================================================================
+
