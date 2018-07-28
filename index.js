e05bc088ea7c817067f051c91efb00c6fec4da52
Michael <Mkoerber7@gmail.com>
2018-07-28 11:40:18 -0400

    Completed Growth of a population/7kyu

--- index.js
+++ index.js
@@ -1495,3 +1495,19 @@ const DNAtoRNA = (dna) => dna.replace(/[T]/g, "U");
 
 //=======================================================================================================
 
+// 7 kyu
+// Growth of a Population
+
+// In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
+
+function nbYear(p0, percent, aug, p) {
+  var countyears=0;
+  while (p0<p) {
+    p0=p0+p0*(percent/100)+aug;
+    countyears+=1;
+  }
+return countyears; 
+}
+
+//=======================================================================================================
+
